5f2089a9fc647a82097764c08145ef6d0487a1ca
Nazmulshuvo123 <nazmulshuvo161199@gmail.com>
2024-09-02 14:51:12 +0600

    find even and odd number

---
+++ odd-even.js
@@ -0,0 +1,33 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+//Find all the odd numbers from 61 to 100.
+
+// for(let n = 61; n <= 100; n++){
+//     // console.log(n);
+//     if( n % 2 === 1){
+//         console.log("The odd number is: ", n);
+//     }
+    
+// }
+
+//Find all the even numbers from 78 to 98.
+
+for( let i = 78; i <= 98; i++){
+    // console.log(i);
+    if( i % 2 === 0){
+        console.log("The even number is : " ,i);
+    }
+}
